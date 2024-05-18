df48ad6cd8e9e2dc96d10b06d801ffe7dfd9dda4
dinanady <152699183+dinanady@users.noreply.github.com>
2024-05-18 23:58:42 +0300

    create class booking

--- wooxtravel/Controllers/reservation.php
+++ wooxtravel/Controllers/reservation.php
@@ -2,3 +2,47 @@
 require_once "../config/session_config.php";
 
 require_once __DIR__ .("/../Model/bookings.php");
+
+require_once __DIR__ .("/../Model/city.php");
+
+
+if($_SERVER['REQUEST_METHOD']==="POST"){
+    $username = $_POST['username'];
+    $phone = $_POST['phone'];
+    $Guests = $_POST['Guests'];
+    $date   = $_POST['checkdate'];
+    $city_id =$_POST["destination"];
+    if(isset( $_SESSION["id"])){
+    if(empty($username)||empty( $phone)||empty($Guests)||empty($date)||empty( $city_id)){
+
+        $_SESSION['empty_reservation']= "one or more fields are empty";
+        header("location: ../reservation.php");
+        exit;
+    }
+
+    else{
+        if(date("Y-M-D") < $date  ){
+    $city = new city("","","","","","");
+    $destination = $city->getcity($city_id);
+    $payment =  $Guests *  $destination['price'];
+    $user_id = $_SESSION["id"];
+    $reservation = new bookings($username, $phone,$Guests,$date ,$destination['name'],$user_id,"pending",$city_id,$payment);
+    $reservation->createreservation();
+
+    
+    header("location: ../index.php");
+    exit;
+        }
+        else {
+           echo "<script>alert('the date must be income ') </script>";  
+        }
+
+    }
+    }
+    else{
+ 
+        header("location: ../auth/login.php");
+        exit;
+
+    }
+}
